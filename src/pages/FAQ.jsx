4bd840760632e1db515be34ec33b9e6720e70b00
Saate123 <saateechindo@gmail.com>
2025-02-26 18:54:37 +0100

    second

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -5,7 +5,7 @@ import Right2 from "../components/Right2";
 
 function FAQ() {
   return (
-    <div className="bg-gray-100 py-12 px-6 md:px-16">
+    <div className="py-12 px-6 md:px-16">
       <div className="max-w-6xl mx-auto flex justify-between flex-col lg:flex-row items-center gap-12 lg:gap-16">
         {/* Left - Image */}
         <div className="w-full lg:w-96 flex justify-center">
